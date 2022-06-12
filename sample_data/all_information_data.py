179c8ef70eae7d90ced8325526c1ea5da2961963
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-06-12 22:56:31 +0600

    create sample data folder for store all information

---
+++ sample_data/all_information_data.py
@@ -0,0 +1,9 @@
+class LoginData(object):
+    EMAIL = "demo@gmail.com"
+    PASSWORD = "123456"
+
+
+class SignUpData(object):
+    NAME = "demo"
+    EMAIL = "demo@gmail.com"
+    PASSWORD = "123456"
