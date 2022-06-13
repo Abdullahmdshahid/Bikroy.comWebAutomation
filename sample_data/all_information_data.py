385469780584484597d447a7986b64ac79f56c5f
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-06-13 23:41:47 +0600

    add setting page data

--- sample_data/all_information_data.py
+++ sample_data/all_information_data.py
@@ -7,3 +7,13 @@ class SignUpData(object):
     NAME = "demo"
     EMAIL = "demo@gmail.com"
     PASSWORD = "123456"
+
+
+class SettingPageData(object):
+    NAME = "demo1"
+    CITY = "Dhaka"
+    SUB_LOCATION = "Aftab nagar"
+    CURRENT_PASSWORD = "123456"
+    NEW_PASSWORD = "654321"
+
+
