69115926d9dc2281361072ff421c495b1bc90d72
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-06-15 17:26:49 +0600

    adding profile page data

--- sample_data/all_information_data.py
+++ sample_data/all_information_data.py
@@ -17,3 +17,15 @@ class SettingPageData(object):
     NEW_PASSWORD = "654321"
 
 
+class ProfilePageData(object):
+    NAME = "demo1"
+    PHONE = "00478522551"
+    GENDER = "Male"
+    DEATH_OF_BIRTH = "01/01/1990"
+    CITY = "Dhaka"
+    SUB_LOCATION = "Aftab nagar"
+    EDUCATION = "High School"
+    JOB = "Teacher"
+    YEARS = "5"
+
+
