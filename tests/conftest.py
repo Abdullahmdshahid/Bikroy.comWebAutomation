01aa91c7896eaa3fed14910fd494e6c9ae88e3b9
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-07-08 00:01:38 +0600

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+import os
 
 @pytest.fixture(scope="class")
 # setup the webdriver
 def setup(request):
 
-    chromeService = Service("F:\ChromeDriver\chromedriver.exe")
+    chromeService = Service(os.getcwd() + r"\ChromeDriver\chromedriver.exe")
     driver = webdriver.Chrome(service=chromeService)
 
-    driver.get("https://bikroy.com/bn")
+    driver.get("https://bikroy.com/")
     driver.maximize_window()
     request.cls.driver = driver
 
