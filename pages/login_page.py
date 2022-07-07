1b4fd78c936338202e56c9a5198e943f46c1f81b
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-07-08 00:37:36 +0600

    sloveing login problem after changing password

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.base_page import BasePage
 from utils.locators import *
 
@@ -17,13 +19,29 @@ class LoginPage(BasePage):
         button = self.driver.find_element(*self.locator.CONTINUE_WITH_EMAIL_BUTTON)
         button.click()
 
-    def login_with_email_and_password(self, email, password):
+    def login_with_email_and_password(self, email, password, newpassword):
         email_text_box = self.find_element(*self.locator.EMAIL_TEXTBOX)
         email_text_box.send_keys(email)
         password_text_box = self.find_element(*self.locator.PASSWORD_TEXTBOX)
         password_text_box.send_keys(password)
         login_button = self.find_element(*self.locator.LOGIN_BUTTON)
         login_button.click()
+        time.sleep(3)
+
+        login_failed_text = self.find_element(*self.locator.LOGIN_FAILED_TEXT)
+
+        # login after change the password
+        if "try again" in login_failed_text.text:
+            self.driver.refresh()
+            self.click_with_email_button()
+            email_text_box = self.find_element(*self.locator.EMAIL_TEXTBOX)
+            email_text_box.send_keys(email)
+
+            password_text_box = self.find_element(*self.locator.PASSWORD_TEXTBOX)
+            password_text_box.send_keys(newpassword)
+
+            login_button = self.find_element(*self.locator.LOGIN_BUTTON)
+            login_button.click()
 
     def get_login_failed_text(self):
         super().wait_element(*self.locator.LOGIN_FAILED_TEXT)
